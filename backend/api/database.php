7bb72a27fe5a59109b7314d0da6fdece38483a1d
psuthe <peter@goodmountain.com>
2019-12-28 14:06:27 -0800

    Update database.php

--- backend/api/database.php
+++ backend/api/database.php
@@ -4,11 +4,11 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', 'P3t3r12');
-//define('DB_PASS', 'P3t3r12$');
+//define('DB_USER', 'root');
+//define('DB_PASS', 'P3t3r12');
+define('DB_USER', 'caddyBiker');
+define('DB_PASS', '4V8q0A^4}cLj');
 define('DB_NAME', 'BIKE_MAINT');
-
 
 function connect()
 {
